ed5473d9535eb0f119f18fd379a8d48b7b8bcc05
Prateek Mishra <pmishra@mdsol.com>
2022-09-15 15:58:07 +0530

    arrow methods

--- basicJs/functions.js
+++ basicJs/functions.js
@@ -1,9 +1,25 @@
-function Add(v1, v2) {
-  console.log(v1 + v2);
+function div(arg1, arg2, arg3) {
+  return arg1 / arg2 / arg3;
 }
 
-function result(v1, v2) {
-  v2(4, 2);
+function multi(arg1, arg2, arg3) {
+  var multi = arg1 * arg2 * arg3;
+  return div(arg1, arg2, multi);
 }
 
-result(3, Add);
+function addition(arg1, arg2, arg3) {
+  var add = arg1 + arg2 + arg3;
+  return multi(arg1, arg2, add);
+}
+
+function subtraction(arg1, arg2) {
+  var sub = arg1 - arg2;
+  return addition(arg1, arg2, sub);
+}
+
+function restult(arg) {
+  var res = arg(25, 5);
+  console.log(res);
+}
+
+restult(subtraction);
